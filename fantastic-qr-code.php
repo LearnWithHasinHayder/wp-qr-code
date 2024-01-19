11b9093b4a20a27f5d310d8b47ea2ed7c876e624
Hasin Hayder <countdraculla@gmail.com>
2024-01-19 22:14:56 +0600

    fantastic qr code

---
+++ fantastic-qr-code.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Plugin Name: Fantastic QR Code
+ * Description: Display a QR code for the current page
+ * Version: 1.0.0
+ * Author: Hasin Hayder
+ * Author URI: http://hasin.me
+ * Plugin URI: http://google.com
+ */
+
+class FQC_Qr_Code {
+    public function __construct() {
+        add_action('init', array($this, 'init'));
+    }
+
+    public function init() {
+        add_filter('the_content', array($this, 'add_qr_code'));
+    }
+
+    public function add_qr_code($content) {
+        $current_link = esc_url(get_permalink());
+        $title = get_the_title();
+        $custom_content = '<div style="border: 1px solid #ddd; padding: 10px; margin: 20px 0;">';
+        // $custom_content .= '<img src="https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl=' . $current_link . '" alt="'.$title.'" />';
+        // $custom_content .="<img src='https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl={$current_link}' alt='{$title}' />";
+        $custom_content .="<img src='https://api.qrserver.com/v1/create-qr-code/?color=ff0000&size=150x150&data={$current_link}' alt='{$title}' />";
+        $custom_content .= '</div>';
+        
+        $content .= $custom_content;
+        return $content;
+    }
+}
+
+new FQC_Qr_Code();
